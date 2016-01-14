de407b0bc32e3c8272629302baf3b9f2d45d844a
Sergei Maertens <sergei@maykinmedia.nl>
2016-01-14 01:09:59 +0100

    Refactor help to @command

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -1,11 +1,12 @@
 from bot.plugins.base import BasePlugin
+from bot.plugins.commands import command
 
 
 class Plugin(BasePlugin):
 
-    def on_message(self, message):
-        if message.content.lower() == '!help':
-            plugins = self.client._method_pool.plugin_handlers.keys()
-            help_messages = [plugin.help for plugin in plugins if hasattr(plugin, 'help')]
-            msg = '\n\n'.join(help_messages)
-            yield from self.client.send_message(message.channel, msg)
+    @command()
+    def help(self, command):
+        plugins = self.client._method_pool.plugin_handlers.keys()
+        help_messages = [plugin.help for plugin in plugins if hasattr(plugin, 'help')]
+        msg = '\n\n'.join(help_messages)
+        yield from command.reply(msg)
