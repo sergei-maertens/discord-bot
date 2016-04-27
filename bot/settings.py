264ef647ce9b53b8a5eb905616296daba4c9fc6f
Sergei Maertens <sergei@maykinmedia.nl>
2016-04-27 16:15:13 +0200

    Move to postgresql instead of sqlite

--- bot/settings.py
+++ bot/settings.py
@@ -51,11 +51,12 @@ LOGGING = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'discord.db',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
